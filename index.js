c87345ab580567a5e565313902485b4c40aa80bb
Troy Lawrence <redirect@gmail.com>
2018-02-08 02:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function takeANumber (katzDeliLine, name) {
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length>0) {
      return `Currently serving ${katzDeliLine.shift()}.`;
-     
   } else {
      return `There is nobody waiting to be served!`;
   }
@@ -21,9 +20,11 @@ function nowServing (katzDeliLine) {
 
 function currentLine (katzDeliLine) {
   if (katzDeliLine.length>0) {
+     let newArray = [];
     for (let i = 1; i<katzDeliLine.length + 1; i++){
-    return `The Line is currently: ${i}. ${katzDeliLine[i-1]}`;
+         newArray.push(`${i}. ${katzDeliLine[i-1]}`);
     }
+    return `The line is currently: ${newArray}`;
   } else { 
     return `The line is currently empty.`;
   }
