ff3ff540f828378ada7090dbef0538403baa7987
Troy Lawrence <redirect@gmail.com>
2018-02-08 02:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber (katzDeliLine, name) {
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length>0) {
-     return `Currently serving ${katzDeliLine[0].shift()}.`;
+     return `Currently serving ${katzDeliLine.shift()}.`;
      
   } else {
      return `There is nobody waiting to be served!`;
